d4f9e292d421fd7ad81a67d42c208ace8395d47e
artembell <artembell305@gmail.com>
2018-01-12 21:37:28 +0300

    Added Login tests

---
+++ Login.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace IamIT.Tests.UI
+{
+    public class Login : LoginBase
+    {
+        [Fact]
+        protected void LoginIncorrectUsernameOrPassword()
+        {
+            UserInput.SendKeys("artembell");
+            PasswordInput.SendKeys("123asdfa");
+
+            Driver.FindElementById("loginBtn").Click();
+
+            if (Driver.FindElementByXPath("//div[contains(normalize-space(), 'Неверное имя пользователя или пароль')]") != null)
+            {
+                throw new Exception("Неверное имя пользователя или пароль");
+            }
+        }
+
+        [Fact]
+        protected void LoginWithEmptyUsernameField()
+        {
+            PasswordInput.SendKeys("123asdfa");
+
+            Driver.FindElementById("loginBtn").Click();
+
+            if (Driver.FindElementByXPath("//span[contains(normalize-space(), 'Это поле не может быть пустым')]") != null)
+            {
+                throw new Exception("Поле ввода имени пользователя не может быть пустым");
+            }
+        }
+
+        [Fact]
+        protected void LoginWithEmptyPasswordField()
+        {
+            UserInput.SendKeys("artembell");
+
+            Driver.FindElementById("loginBtn").Click();
+
+            if (Driver.FindElementByXPath("//span[contains(normalize-space(), 'Это поле не может быть пустым')]") != null)
+            {
+                throw new Exception("Поле ввода пароля не может быть пустым");
+            }
+        }
+
+        [Fact]
+        protected void LoginValid()
+        {
+            UserInput.SendKeys("artembell");
+            PasswordInput.SendKeys("123qweqwe");
+
+            if (Driver.FindElementByClassName("myCoursesTitle") == null)
+            {
+                throw new Exception("По непонятным причинам не удалось войти");
+            }
+        }
+    }
+}
---
+++ LoginBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace IamIT.Tests.UI
+{
+    public class LoginBase : Base
+    {
+        public IWebElement UserInput, PasswordInput;
+        public LoginBase()
+        {
+            Driver.FindElementById("signUpBtn").Click();
+            UserInput = Driver.FindElementById("usernameInput");
+            PasswordInput = Driver.FindElementById("passwordInput");
+        }
+    }
+}
